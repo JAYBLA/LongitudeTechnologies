4ae5f2adc3e4325fcbb51e391588f997c0e06511
JAYBLA <jayblaenterprises@gmail.com>
2022-02-24 12:42:01 +0300

    homepage

--- app/Views/layouts/includes/footer.php
+++ app/Views/layouts/includes/footer.php
@@ -1,167 +1,69 @@
-<footer class="footer_section bg_gray clearfix">
-			<div class="container">
+<footer class="footer_section">
+    <div class="topFooter">
+        <div class="container">
+            <div class="footer_widget_area">
+                <div class="row justify-content-lg-between">
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="widget footer_useful_links">
+                            <h3 class="footer_widget_title">Quick Links</h3>
+                            <p>Sed ut persiatis unde omnis iste natusec error sit voluptatem accusantium</p>
+                        </div>
+                    </div>
 
-				<div class="footer_brand_area border_bottom clearfix">
-					<div class="row align-items-center justify-content-lg-between">
-						<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-							<div class="brand_logo">
-								<a href="#!">
-									<img src="assets/images/logo/logo_02_1x.png" srcset="assets/images/logo/logo_02_2x.png 2x" alt="logo_not_found">
-								</a>
-							</div>
-						</div>
-
-						<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-							<div class="brands_list">
-								<div class="logo_image">
-									<a href="#!">
-										<img src="assets/images/brands/img_01.png" alt="logo_not_found">
-									</a>
-								</div>
-
-								<div class="logo_image">
-									<a href="#!">
-										<img src="assets/images/brands/img_02.png" alt="logo_not_found">
-									</a>
-								</div>
-
-								<div class="logo_image">
-									<a href="#!">
-										<img src="assets/images/brands/img_03.png" alt="logo_not_found">
-									</a>
-								</div>
-
-								<div class="logo_image">
-									<a href="#!">
-										<img src="assets/images/brands/img_04.png" alt="logo_not_found">
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="footer_widget_area clearfix">
-					<div class="row justify-content-lg-between">
-						<div class="col-lg-2 col-md-4 col-sm-12 col-xs-12">
-							<div class="widget footer_useful_links">
-								<h3 class="footer_widget_title">Quick Links</h3>
-								<ul class="ul_li_block clearfix">
-									<li><a href="#!">Company History</a></li>
-									<li><a href="#!">Exclusive Team </a></li>
-									<li><a href="#!">Latest News</a></li>
-									<li><a href="#!">Recent Product</a></li>
-									<li><a href="#!">Win Awards</a></li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-lg-2 col-md-4 col-sm-12 col-xs-12">
-							<div class="widget footer_useful_links">
-								<h3 class="footer_widget_title">Services</h3>
-								<ul class="ul_li_block clearfix">
-									<li><a href="#!">Design & Branding</a></li>
-									<li><a href="#!">3D Design & Printing</a></li>
-									<li><a href="#!">Offset Printing</a></li>
-									<li><a href="#!">Business Printing</a></li>
-									<li><a href="#!">Corporate Printing</a></li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-							<div class="widget recent_works_list">
-								<h3 class="footer_widget_title">Recent News</h3>
-								<ul class="ul_li_block clearfix">
-									<li>
-										<div class="small_blog clearfix">
-											<a class="item_image" target="_blank" href="blog_details.html">
-												<img src="assets/images/recent_works/img_01.png" alt="image_not_found">
-											</a>
-											<div class="item_content">
-												<h3 class="item_title">
-													<a target="_blank" href="blog_details.html">
-														Web Designers Can Help Restaura Move Into
-													</a>
-												</h3>
-												<span class="post_date"><i class="fa fa-calendar-alt"></i> 25 Nov 2020</span>
-											</div>
-										</div>
-									</li>
-
-									<li>
-										<div class="small_blog clearfix">
-											<a class="item_image" target="_blank" href="blog_details.html">
-												<img src="assets/images/recent_works/img_02.png" alt="image_not_found">
-											</a>
-											<div class="item_content">
-												<h3 class="item_title">
-													<a target="_blank" href="blog_details.html">
-														Building Facial Recognits Web Application React
-													</a>
-												</h3>
-												<span class="post_date"><i class="fa fa-calendar-alt"></i> 25 Nov 2020</span>
-											</div>
-										</div>
-									</li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-lg-3 col-md-12 col-sm-12 col-xs-12">
-							<div class="widget footer_contact_form">
-								<h3 class="footer_widget_title">Contact Us</h3>
-								<p class="mb_30">
-									Sed ut persiatis unde omnis iste natusec error sit voluptatem accusantium
-								</p>
-								<form action="#">
-									<div class="form_item">
-										<input type="email" name="email" placeholder="Enter Your Email">
-										<button type="submit" class="submit_btn"><i class="fa fa-arrow-right"></i></button>
-									</div>
-								</form>
-								<span class="footer_hotline"><i class="fa fa-phone"></i> +012 (345) 678 99</span>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="footer_bottom_boxed bg_white clearfix">
-					<div class="row align-items-center justify-content-lg-between">
-						<div class="col-lg-6 col-md-6">
-							<p class="copyright_text mb-0">© Copyright 2020 All Right Reserved Design By <a class="author_links" target="_blank" href="https://themeforest.net/user/bdevs">BDevs</a></p>
-						</div>
-
-						<div class="col-lg-6 col-md-6">
-							<ul class="circle_social_links ul_li_right clearfix">
-								<li>
-									<a class="bg_facebook" href="#!">
-										<i class="fa fa-facebook-f"></i>
-										<i class="fa fa-facebook-f"></i>
-									</a>
-								</li>
-								<li>
-									<a class="bg_twitter" href="#!">
-										<i class="fa fa-twitter"></i>
-										<i class="fa fa-twitter"></i>
-									</a>
-								</li>
-								<li>
-									<a class="bg_youtube" href="#!">
-										<i class="fa fa-youtube"></i>
-										<i class="fa fa-youtube"></i>
-									</a>
-								</li>
-								<li>
-									<a class="bg_linkedin" href="#!">
-										<i class="fa fa-linkedin"></i>
-										<i class="fa fa-linkedin"></i>
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-
-			</div>
-		</footer>
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="widget footer_useful_links">
+                            <h3 class="footer_widget_title">Useful Links</h3>
+                            <ul class="ul_li_block">
+                                <li><a href="#!">Design & Branding</a></li>
+                                <li><a href="#!">3D Design & Printing</a></li>
+                                <li><a href="#!">Offset Printing</a></li>
+                                <li><a href="#!">Business Printing</a></li>
+                                <li><a href="#!">Corporate Printing</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="widget footer_contact_form">
+                            <ul class="circle_social_links ul_li_right">
+                                <li>
+                                    <a class="bg_facebook" href="#!">
+                                        <i class="fa fa-facebook-square fa_30 text-dark"></i>
+                                        <i class="fa fa-facebook-square fa_30 text-dark"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="bg_twitter" href="#!">
+                                        <i class="fa fa-twitter fa_30 text-dark"></i>
+                                        <i class="fa fa-twitter fa_30 text-dark"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="bg_youtube" href="#!">
+                                        <i class="fa fa-instagram fa_30 text-dark"></i>
+                                        <i class="fa fa-instagram fa_30 text-dark"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="bg_linkedin" href="#!">
+                                        <i class="fa fa-linkedin fa_30 text-dark"></i>
+                                        <i class="fa fa-linkedin fa_30 text-dark"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottomFooter">
+        <div class="footer_bottom_boxed container">
+            <div class="row align-items-center justify-content-lg-between">
+                <div class="col-12">
+                    <p class="copyright_text mb-0">© Copyright 2020 All Right Reserved Design By <a class="author_links"
+                            target="_blank" href="https://themeforest.net/user/bdevs">BDevs</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
