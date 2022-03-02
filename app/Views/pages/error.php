76d09175e5c535324975c93175c8bf4f037452bc
JAYBLA <jayblaenterprises@gmail.com>
2022-03-02 20:47:08 +0300

    margins on error

--- app/Views/pages/error.php
+++ app/Views/pages/error.php
@@ -5,7 +5,7 @@
 
 <?= $this->section('content') ?>
 <section class="innerTopMargin"></section>
-<div class="container vh-100">
+<div class="container m-auto vh-100">
 <h1 class="color-primary">404 Page Not Found</h1>
 </div>
 <?= $this->endSection() ?>
